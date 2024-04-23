48d56bdf6111e2ae81dea6acc73eb7525673dd9f
jojooah <my_sg1@naver.com>
2024-04-23 19:20:20 +0900

    추가

---
+++ src/main/java/com/flab/jojoboard/JojoBoardApplication.java
@@ -0,0 +1,13 @@
+package com.flab.jojoboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JojoBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JojoBoardApplication.class, args);
+    }
+
+}
